b9ad7c951364f2ae80397ceb126e8a9e2866837c
Zhaoxiang-Shen <z3shen@eng.ucsd.edu>
2021-12-02 14:51:06 +0100

    Include the abs(n_as) in the output

--- SNoIn/TangentMethod.py
+++ SNoIn/TangentMethod.py
@@ -7,20 +7,29 @@ on the substrate which must defined by the same mesh as the membrane.
 
 from dolfin import *
 
-def d_tangent_plane(spline_sub, u_sub, y_shell, ux_shell, uz_shell, shift_sub):
+def unit(v):
     """
-    "u_sub" is the displacement field of the substrate surface.
-    "y_shell" is in the current configuration, "ux/uz_shell" is the displacement.
-    "shift_sub" is the downward shift of the substrate.
+    Normalize the vector ``v``.
+    """
+    return v/sqrt(inner(v,v))
+
+def d_tangent_plane(spline_sub, u_sub, y_shell, ux_shell, uz_shell, shift_sub, a2):
+    """
+    "u_sub" is the displacement field of the substrate surface,
+    "y_shell" is in the current configuration, "ux/uz_shell" is the displacement,
+    "shift_sub" is the downward shift of the substrate,
+    "a2" is the normal vector at "x".
     """
     # vector
     ux = spline_sub.grad(u_sub)[0]
     uz = spline_sub.grad(u_sub)[1]
-    normal_u_sub = as_vector([ux,uz,Constant(-1)])
+    n_u_sub = as_vector([ux, uz, Constant(-1)])  # Normal vector of the tangent plane
     y_vec = as_vector([0,0,Constant(1)])  # A vertical vector
     # angle
-    cos_theta = inner(normal_u_sub,y_vec)/(sqrt(inner(normal_u_sub,normal_u_sub))*sqrt(inner(y_vec,y_vec)))
+    cos_theta = inner(n_u_sub, y_vec) / (sqrt(inner(n_u_sub, n_u_sub)) * sqrt(inner(y_vec, y_vec)))
     cos_theta = sqrt(cos_theta**2)
+    # shift coefficient for S atoms
+    n_a2 = inner(unit(n_u_sub), a2)
     # distance
-    d = (y_shell - (u_sub + ux * ux_shell + uz * uz_shell - shift_sub)) * cos_theta
-    return d
+    d_Mo = (y_shell - (u_sub + ux * ux_shell + uz * uz_shell - shift_sub)) * cos_theta
+    return d_Mo, sqrt(n_a2**2)
