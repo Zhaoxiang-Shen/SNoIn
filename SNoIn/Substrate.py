c38f1e80b822ede060ba1bbadbe53a59c26ae1b2
Zhaoxiang Shen <z3shen@eng.ucsd.edu>
2022-01-12 17:54:23 +0100

    Add a cosSub for large R
    The new cosSub consider neighbouring holes for the boudary smoothness.

--- SNoIn/Substrate.py
+++ SNoIn/Substrate.py
@@ -2,6 +2,7 @@ from dolfin import *
 from tIGAr import *
 from tIGAr.NURBS import *
 from tIGAr.BSplines import *
+from numpy import array
 
 def dist(x, z, center):
     d = ((x - center[0]) ** 2 + (z - center[1]) ** 2) ** 0.5
@@ -36,6 +37,27 @@ class SubstrateLibrary:
         wr = Constant(1/R)
         cos_r = (cos(pi * wr * ((self.x[0] - center[0]) ** 2 + (self.x[1] - center[1]) ** 2) ** 0.5) + 1)/2
         u_sub = self.spline.project(zero_region * Constant(-A) * cos_r ** alpha
+                                        + Constant(-shift), rationalize=False)
+        return u_sub
+
+    def cosSub_largeR(self,center,L,R,A,alpha,shift=0):
+        # initialize the center hole
+        d = dist(self.x[0], self.x[1], center)
+        zero_region = conditional(gt(d, Constant(R)), 0, Constant(1))
+        wr = Constant(1/R)
+        cos_r_0 = (cos(pi * wr * ((self.x[0] - center[0]) ** 2 + (self.x[1] - center[1]) ** 2) ** 0.5) + 1)/2
+        # Sum neighbouring holes
+        sub_sum = zero_region * cos_r_0 ** alpha
+        for i in range(6):
+            theta_i = i*pi/3
+            hole_shift_i = L * (cos(theta_i)*array([1,0])+sin(theta_i)*array([0,1]))
+            center_i =  center+hole_shift_i
+            d_i = dist(self.x[0], self.x[1],center_i)
+            zero_region_i = conditional(gt(d_i, Constant(R)), 0, Constant(1))
+            cos_r_i = (cos(pi * wr * ((self.x[0] - center_i[0]) ** 2 + (self.x[1] - center_i[1]) ** 2) ** 0.5) + 1) / 2
+            sub_sum += zero_region_i * cos_r_i ** alpha
+
+        u_sub = self.spline.project(Constant(-A) * sub_sum
                                         + Constant(-shift), rationalize=False)
         return u_sub
 
